181ac1b681c4acb5a6bac286fb2bc41d3afbbcec
Alex TOURNANT <alex_tournant@ens.univ-artois.fr>
2024-10-24 11:29:56 +0200

    add file inpainting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 from routes.text_to_image_routes import textToImage_routes
 # from routes.ImageToImageFinetuning import imageToImageFinetuning_routes
 # from routes.InpaintingFinetuning import inpaintingFinetuning_routes
-# from routes.Inpainting import inpainting_routes
+from routes.inpainting_routes import inpainting_routes
 from routes.generate_manualy_mask_routes import generateManualyMask_routes
 
 
@@ -19,7 +19,7 @@ app.register_blueprint(textToImage_routes, url_prefix='/textToImage')
 #                        url_prefix='/imageToImageFinetuning')
 # app.register_blueprint(inpaintingFinetuning_routes,
 #                        url_prefix='/inpaintingFinetuning')
-# app.register_blueprint(inpainting_routes, url_prefix='/inpainting')
+app.register_blueprint(inpainting_routes, url_prefix='/inpainting')
 app.register_blueprint(generateManualyMask_routes,
                        url_prefix='/generateManualyMask')
 
