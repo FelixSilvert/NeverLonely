da9522f685ca4be16b6ebe2c020642fbf43caa3c
Alex TOURNANT <alex_tournant@ens.univ-artois.fr>
2024-10-24 11:26:00 +0200

    add file image to image finetuning

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask
 # from routes.auth import auth_routes
 # from routes.image_to_image import imageToImage_routes
 from routes.text_to_image_routes import textToImage_routes
-# from routes.ImageToImageFinetuning import imageToImageFinetuning_routes
+from routes.image_to_image_finetuning_routes import image_to_image_finetuning_routes
 # from routes.InpaintingFinetuning import inpaintingFinetuning_routes
 # from routes.Inpainting import inpainting_routes
 from routes.generate_manualy_mask_routes import generateManualyMask_routes
@@ -15,8 +15,8 @@ app = Flask(__name__)
 # app.register_blueprint(auth_routes, url_prefix='/auth')
 # app.register_blueprint(imageToImage_routes, url_prefix='/imageToImage')
 app.register_blueprint(textToImage_routes, url_prefix='/textToImage')
-# app.register_blueprint(imageToImageFinetuning_routes,
-#                        url_prefix='/imageToImageFinetuning')
+app.register_blueprint(image_to_image_finetuning_routes,
+                       url_prefix='/imageToImageFinetuning')
 # app.register_blueprint(inpaintingFinetuning_routes,
 #                        url_prefix='/inpaintingFinetuning')
 # app.register_blueprint(inpainting_routes, url_prefix='/inpainting')
